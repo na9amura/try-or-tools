6b5a7c188f5e6b4daa079b0440fc9ed80f247346
na9amura <4410504+na9amura@users.noreply.github.com>
2022-05-05 17:07:33 +0900

    refactor

--- group/solver.py
+++ group/solver.py
@@ -4,38 +4,27 @@ import string
 
 
 class Problem:
-    df: pd.DataFrame
-    len_groups: int
-    prev_group_mates = dict()
     M: list[int]
     G: list[int]
     MG: list[(int, int)]
+    prev_group_mates = dict()
+    swe_list: list[int]
     X: pulp.LpVariable.dicts
 
-    def __init__(self, file: string, *, members_per_group=3, prev_group=pd.DataFrame([])):
-        self.df = pd.read_csv(file)
-        len_members = len(self.df)
-        self.len_groups = int(len_members / members_per_group)
+    def __init__(self, members: list[int], groups: list[int], swe_list: list[int]):
+        self.M = members
+        self.G = groups
+        self.MG = [(m, g) for m in members for g in groups]
+        self.swe_list = swe_list
 
-        for g, data in prev_group.groupby('group'):
-            members = data['ID'].to_list()
-            for name in members:
-                self.prev_group_mates[name] = members
-
-    def run(self, ) -> pd.DataFrame:
+    def run(self) -> (pulp.LpStatus, dict):
         problem = self.__create_problem()
-        problem.solve()
+        status = problem.solve()
 
-        result_df = self.df.copy()
-        result_df['group'] = result_df['ID'].map({m: g for m in self.M for g in self.G if self.X[m, g].value() == 1})
-
-        return result_df
+        return status, {m: g for m in self.M for g in self.G if self.X[m, g].value() == 1}
 
     def __create_problem(self) -> pulp.LpProblem:
         # Variables
-        self.M = self.df['ID'].tolist()
-        self.G = list(string.ascii_uppercase[:self.len_groups])
-        self.MG = [(m, g) for m in self.M for g in self.G]
         self.X = pulp.LpVariable.dicts('X', self.MG, cat=pulp.LpBinary)
 
         problem = pulp.LpProblem("Group", pulp.LpMaximize)
@@ -50,13 +39,31 @@ class Problem:
             problem += pulp.lpSum(self.X[m, g] for m in self.M) <= 4
 
         # 3. Max members of SwE must be 2
-        swe = self.df[self.df['SwE'] == 1]['ID'].tolist()
         for g in self.G:
-            problem += pulp.lpSum(self.X[m, g] for m in swe) <= 2
+            problem += pulp.lpSum(self.X[m, g] for m in self.swe_list) <= 2
 
         # 4. Avoid previous group members
-        for n, members in self.prev_group_mates.items():
-            for g in self.G:
-                problem += pulp.lpSum(self.X[m, g] for m in members) <= 2
+        if len(self.prev_group_mates) > 0:
+            for n, members in self.prev_group_mates.items():
+                for g in self.G:
+                    problem += pulp.lpSum(self.X[m, g] for m in members) <= 2
 
         return problem
+
+
+def create_lp_problem(df, *, members_per_group=3, prev_group=pd.DataFrame([])) -> Problem:
+    len_members = len(df)
+    len_groups = int(len_members / members_per_group)
+    members = df['ID'].tolist()
+    groups = list(string.ascii_uppercase[:len_groups])
+
+    swe_list = df[df['Role_Engineer'] == 1]['ID'].tolist()
+
+    prev_group_mates = dict()
+    if len(prev_group) > 0:
+        for g, data in prev_group.groupby('Group'):
+            members = data['ID'].to_list()
+            for name in members:
+                prev_group_mates[name] = members
+
+    return Problem(members, groups, swe_list)
